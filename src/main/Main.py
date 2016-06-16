43ed0b90d9e6dbf930841d5d91045737a1ee9651
Kevin Bienvenu <bienvenu.kevin@sfr.fr>
2016-06-16 10:52:47 +0100

    preparation du graphcomplet via gitignore

--- src/main/Main.py
+++ src/main/Main.py
@@ -5,10 +5,13 @@ Created on 25 avr. 2016
 @author: Kévin Bienvenu
 '''
 import KeywordSubset, Constants
+from main import GraphPreprocess
 
 
 
-KeywordSubset.extractSubset(toPrint=True)
+# KeywordSubset.extractSubset(toPrint=True)
+
+GraphPreprocess.extractGraphFromSubset("graphcomplet", Constants.pathCodeNAF)
 
 # KeywordSubset.createAllNAFSubset(200)
 
