ac5f42d053f3216826cb7f6607d3d001f881c1f8
Kevin Bienvenu <bienvenu.kevin@sfr.fr>
2016-07-05 11:17:32 +0100

    creation du pipeline graph

--- src/main/MainKeyword.py
+++ src/main/MainKeyword.py
@@ -5,45 +5,93 @@ Created on 25 avr. 2016
 @author: Kévin Bienvenu
 '''
 
-import codecs
 import os
-import pandas as pd
+import time
 
 import  Constants
 import IOFunctions
 from main import KeywordSelector
+import pandas as pd
+
 
 def main(arg):
-    if arg=="extract subset":
-        codeNAF = ""
+    if arg=="compute graph pipeline":
+        # COMPUTING GRAPH
+        print "COMPUTING COMPLETE GRAPH PIPELINE"
+        print ""
         n = 10000
-        IOFunctions.extractSubset(codeNAF, n)
+        startingPhase = 0
+        
+        # Step 0 : creating a subset of size n
+        if(startingPhase<=0):
+            startTime = time.time()
+            print "Step 0 : creating a subset of size n=",str(n)
+            codeNAF = ""
+            IOFunctions.extractSubset(codeNAF, n)
+            Constants.printTime(startTime)
+            print ""
 
-    elif arg=="compute graph":
-        subsetname = "graphcomplet_size_10000"
-        path = Constants.pathCodeNAF
-        localKeywords = True
-        IOFunctions.extractGraphFromSubset(subsetname, path, localKeywords)
+        # Step 1 : computing the graph for this subset
+        if(startingPhase<=1):
+            startTime = time.time()
+            print "Step 1 : computing the graph for this subset"
+            subsetname = "graphcomplet_size_"+str(n)
+            path = Constants.pathCodeNAF
+            localKeywords = False
+            print "   computing graph..."
+            IOFunctions.extractGraphFromSubset(subsetname, path, localKeywords)
+            print "   computing keywords..."
+            IOFunctions.extractKeywordsFromGraph(subsetname, path)
+            Constants.printTime(startTime)
+            print ""
 
-    elif arg=="create all NAF":
-        # step 0 : importing list of codeNAF
-        codeNAFs =IOFunctions.importListCodeNAF()
-        # step 1 : creating all subsets
-        compt = Constants.Compt(codeNAFs, 1, False)
-        for codeNAF in codeNAFs:
-            compt.updateAndPrint()
-            IOFunctions.extractSubset(codeNAF, n, path = Constants.pathCodeNAF, toPrint=True)
-        # step 2 : creating all graph
-        compt = Constants.Compt(codeNAFs, 1, False)
-        for codeNAF in codeNAFs:
-            compt.updateAndPrint()
-            IOFunctions.extractGraphFromSubset("subset_NAF_"+codeNAF, Constants.pathCodeNAF)
-        # step 3 : creating all keywords
-            os.chdir(Constants.pathCodeNAF)
-            for directory in os.listdir("."):
-                if directory[0]=="s":
-                    IOFunctions.extractKeywordsFromGraph(directory, Constants.pathCodeNAF)
-    
+        # Step 2 : creating subset for all NAF
+        if(startingPhase<=2):
+            startTime = time.time()
+            print "Step 2 : creating subset for all NAF"
+            codeNAFs = IOFunctions.importListCodeNAF()
+            compt = Constants.Compt(codeNAFs, 1, False)
+            for codeNAF in codeNAFs:
+                compt.updateAndPrint()
+                IOFunctions.extractSubset(codeNAF, n, path = path, toPrint=True)
+            Constants.printTime(startTime)
+            print ""
+        
+        # Step 3 : computing graph for all code NAF, using keywords from Step 0-1
+        if(startingPhase<=3):
+            startTime = time.time()
+            print "Step 3 : computing graph for all code NAF, using keywords from Step 0-1"
+            compt = Constants.Compt(codeNAFs, 1, False)
+            for codeNAF in codeNAFs:
+                compt.updateAndPrint()
+                IOFunctions.extractGraphFromSubset("subset_NAF_"+codeNAF, path)
+            Constants.printTime(startTime)
+            print ""
+        
+        # Step 4 : extracting keywords for all NAF from graphs of Step 3
+        if(startingPhase<=4):
+            startTime = time.time()
+            print "Step 4 : extracting keywords for all NAF from graphs of Step 3"
+            compt = Constants.Compt(codeNAFs, 1, False)
+            for codeNAF in codeNAFs:
+                compt.updateAndPrint()
+                os.chdir(path)
+                for directory in os.listdir("."):
+                    if directory[0]=="s":
+                        IOFunctions.extractKeywordsFromGraph(directory, path)
+            Constants.printTime(startTime)
+            print ""
+        
+        # Step 5 : compute complete graph using local keywords
+        if(startingPhase<=5):
+            startTime = time.time()
+            print "Step 5 : compute complete graph using local keywords"
+            subsetname = "graphcomplet"
+            localKeywords = True
+            IOFunctions.extractGraphFromSubset(subsetname, path, localKeywords)
+            Constants.printTime(startTime)
+            print ""
+        
     elif arg=="main pipeline":
         # Main Pipeline
         des = [
